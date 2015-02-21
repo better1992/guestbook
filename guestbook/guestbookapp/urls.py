a2e0734859611574a955119b32bcedeecc669181
better1992 <vthanhtam42@gmail.com>
2015-02-21 13:48:18 +0700

    Update urls.py

--- guestbook/guestbookapp/urls.py
+++ guestbook/guestbookapp/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls.defaults import *
-from guestbookapp.views import MainView, SignView, GreetingEditView
-
+from guestbookapp.views import MainView, SignView, GreetingEditView, GreetingDeleteView
 
 urlpatterns = patterns('',
 
     url(r"^sign/$", SignView.as_view(), name="sign"),
     url(r"^$", MainView.as_view(), name="main"),
     url(r"^edit/$", GreetingEditView.as_view(), name='edit'),
-
+    url(r"^delete/$", GreetingDeleteView.as_view(),name='delete')
 )
