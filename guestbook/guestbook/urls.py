6b95309eb88367ed509f006ae908a1aaebe6e530
better1992 <vthanhtam42@gmail.com>
2015-02-25 09:38:01 +0700

    Update urls.py

--- guestbook/guestbook/urls.py
+++ guestbook/guestbook/urls.py
@@ -1,10 +1,5 @@
 from django.conf.urls import patterns, include, url
-<<<<<<< HEAD
-from django.contrib import admin
-from guestbookapp.views import MainView,SignView,GreetingEditView
-=======
 
->>>>>>> origin/feature/django-refactoring
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'guestbook.views.home', name='home'),
