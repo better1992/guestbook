afb89012db9050a690bb40dacedcada8d81c4d10
better1992 <vthanhtam42@gmail.com>
2015-03-11 14:51:31 +0700

    Update urls.py

--- guestbook/guestbook/urls.py
+++ guestbook/guestbook/urls.py
@@ -3,4 +3,4 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
                        url(r"^$", include('guestbookapp.urls')),
                        url(r'^guestbookapp/', include('guestbookapp.urls'))
-        )
+)
