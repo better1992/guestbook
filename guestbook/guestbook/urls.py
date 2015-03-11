8915bb25a82557186a28b7f9b15c4023744d5f88
better1992 <vthanhtam42@gmail.com>
2015-03-11 14:01:51 +0700

    change some code

--- guestbook/guestbook/urls.py
+++ guestbook/guestbook/urls.py
@@ -1,13 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'guestbook.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-
-	url(r"^$", include('guestbookapp.urls')),
-	url(r'^guestbookapp/', include('guestbookapp.urls'))
-
-	# url(r'^sign/$', sign_post),
-) 
+                       url(r"^$", include('guestbookapp.urls')),
+                       url(r'^guestbookapp/', include('guestbookapp.urls'))
+        )
