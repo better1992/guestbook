664e001ec30a9fd833cfe70a32c73261aec39761
better1992 <vthanhtam42@gmail.com>
2015-03-17 18:01:13 +0700

    change some code

--- guestbook/guestbookapp/api.py
+++ guestbook/guestbookapp/api.py
@@ -12,132 +12,122 @@ from models import Greeting
 from views import SignForm
 
 class JSONResponseMixin(object):
-    def render_to_response(self, context, **response_kwargs):
-        return self.get_json_response(self.convert_context_to_json(context), **response_kwargs)
+	def render_to_response(self, context, **response_kwargs):
+		return self.get_json_response(self.convert_context_to_json(context), **response_kwargs)
 
-    def get_json_response(self, content, **httpresponse_kwargs):
-        return HttpResponse(content, content_type='application/json', **httpresponse_kwargs)
+	def get_json_response(self, content, **httpresponse_kwargs):
+		return HttpResponse(content, content_type='application/json', **httpresponse_kwargs)
 
-    def convert_context_to_json(self, context):
-        return json.dumps(context)
+	def convert_context_to_json(self, context):
+		return json.dumps(context)
 
 
 class GreetingService(JSONResponseMixin, BaseListView, FormView):
-    form_class = SignForm
+	form_class = SignForm
 
-    # handle GET request: get list messages and return as JSON
-    def get(self, *args, **kwargs):
-        cursor = self.request.GET.get("cursor", None)
-        guestbook_name = kwargs.get("guestbook_name")
-        try:
-            greetings, next_cursor, more = Greeting.get_latest(guestbook_name, 20, cursor)
-        except StandardError:
-            return Http404
-        lst_greeting = []
-        for greeting in greetings:
-            item = {
-                'id': str(greeting.key.id()),
-                'author': str(greeting.author),
-                'content': greeting.content,
-                'date': str(greeting.date)
-            }
-            lst_greeting.append(item)
+	# handle GET request: get list messages and return as JSON
+	def get(self, *args, **kwargs):
+		cursor = self.request.GET.get("cursor", None)
+		guestbook_name = kwargs.get("guestbook_name")
+		try:
+			greetings, next_cursor, more = Greeting.get_latest(guestbook_name, 20, cursor)
+		except StandardError:
+			return Http404
+		lst_greeting = [greeting.to_dict() for greeting in greetings]
 
-        data = {
-            "guestbook_name": guestbook_name,
-            "greetings": lst_greeting,
-            "more": more,
-            "next_cursor": str(next_cursor.urlsafe())
-        }
-        self.object = data
-        return super(GreetingService, self).get(self.request, *args, **kwargs)
+		if next_cursor is None:
+			next_cursor_url = ''
+		else:
+			next_cursor_url = str(next_cursor.urlsafe())
 
-    def get_queryset(self):
-        return self.object
+		data = {
+			"guestbook_name": guestbook_name,
+			"greetings": lst_greeting,
+			"more": more,
+			"next_cursor": next_cursor_url
+		}
+		self.object = data
+		return super(GreetingService, self).get(self.request, *args, **kwargs)
 
-    # handle POST request: create message and return HttpStatus code
-    def post(self, request, *args, **kwargs):
-        return super(GreetingService, self).post(self, request, *args, **kwargs)
+	def get_queryset(self):
+		return self.object
 
-    @staticmethod
-    def form_valid(form):
-        guestbook_name = form.cleaned_data.get('guestbook_name')
-        dict_parameter = {
-            'guestbook_name': guestbook_name,
-            'greeting_message': form.cleaned_data.get('greeting_message')
-        }
-        if Greeting.put_from_dict(dict_parameter):
-            return HttpResponse(status=204)
-        else:
-            return HttpResponse(status=404)
+	# handle POST request: create message and return HttpStatus code
+	def post(self, request, *args, **kwargs):
+		return super(GreetingService, self).post(self, request, *args, **kwargs)
 
-    @staticmethod
-    def form_invalid():
-        return HttpResponse(status=400)
+	@staticmethod
+	def form_valid(form):
+		guestbook_name = form.cleaned_data.get('guestbook_name')
+		dict_parameter = {
+			'guestbook_name': guestbook_name,
+			'greeting_message': form.cleaned_data.get('greeting_message')
+		}
+		if Greeting.put_from_dict(dict_parameter):
+			return HttpResponse(status=204)
+		else:
+			return HttpResponse(status=404)
+
+	@staticmethod
+	def form_invalid():
+		return HttpResponse(status=400)
 
 
 class GreetingManageService(JSONResponseMixin, BaseDetailView, FormView):
-    form_class = SignForm
+	form_class = SignForm
 
-    # handle GET request: get a message and return HttpStatus code
-    def get_object(self):
-        greeting_id = self.kwargs.get("id")
-        guestbook_name = self.kwargs.get("guestbook_name")
+	# handle GET request: get a message and return HttpStatus code
+	def get_object(self):
+		greeting_id = self.kwargs.get("id")
+		guestbook_name = self.kwargs.get("guestbook_name")
 
-        greeting = Greeting.get_greeting(guestbook_name, greeting_id)
-        if greeting is None:
-            return Http404
+		greeting = Greeting.get_greeting(guestbook_name, greeting_id)
+		if greeting is None:
+			return Http404
 
-        data = {
-            "greeting_id": str(greeting_id),
-            "content": greeting.content,
-            "date": str(greeting.date),
-            "updated_by": str(greeting.author),
-            "guestbook_name": guestbook_name
-        }
-        return data
+		return greeting.to_dict()
 
-    # handle PUT request: Edit a message and return HttpStatus code
-    def put(self, request):
-        request.POST = json.loads(request.body)
-        form_class = self.get_form_class()
-        form = self.get_form(form_class)
-        if form.is_valid():
-            return self.form_valid(form)
-        else:
-            return self.form_invalid()
+	# handle PUT request: Edit a message and return HttpStatus code
+	def put(self, request):
+		request.POST = json.loads(request.body)
+		form_class = self.get_form_class()
+		form = self.get_form(form_class)
+		if form.is_valid():
+			return self.form_valid(form)
+		else:
+			return self.form_invalid()
 
-    def form_valid(self, form):
-        if users.get_current_user():
-            updated_by = users.get_current_user().nickname()
-        else:
-            updated_by = None
-        dict_parameter = {
-            'guestbook_name': self.kwargs.get("guestbook_name"),
-            'greeting_id': self.kwargs.get("greeting_id"),
-            'content': form.cleaned_data["greeting_message"],
-            'updated_by': updated_by,
-            'updated_day': str(datetime.datetime.now().date())
-        }
+	def form_valid(self, form):
+		if users.get_current_user():
+			updated_by = users.get_current_user().nickname()
+		else:
+			updated_by = None
+		dict_parameter = {
+			'guestbook_name': self.kwargs.get("guestbook_name"),
+			'greeting_id': self.kwargs.get("greeting_id"),
+			'content': form.cleaned_data["greeting_message"],
+			'updated_by': updated_by,
+			'updated_day': str(datetime.datetime.now().date())
+		}
 
-        if Greeting.edit_greeting(dict_parameter):
-            return HttpResponse(status=204)
-        else:
-            return HttpResponse(status=404)
+		if Greeting.edit_greeting(dict_parameter):
+			return HttpResponse(status=204)
+		else:
+			return HttpResponse(status=404)
 
-    @staticmethod
-    def form_invalid():
-        return HttpResponse(status=400)
+	@staticmethod
+	def form_invalid():
+		return HttpResponse(status=400)
 
-    # handle DELETE request: Delete a message and return HttpStatus code
-    @staticmethod
-    def delete(**kwargs):
-        dict_parameter = {
-            'greeting_id': kwargs.get("greeting_id"),
-            'guestbook_name': kwargs.get("guestbook_name")
-        }
+	# handle DELETE request: Delete a message and return HttpStatus code
+	@staticmethod
+	def delete(**kwargs):
+		dict_parameter = {
+			'greeting_id': kwargs.get("greeting_id"),
+			'guestbook_name': kwargs.get("guestbook_name")
+		}
 
-        if Greeting.delete_greeting(dict_parameter):
-            return HttpResponse(status=204)
-        else:
-            return HttpResponse(status=404)
+		if Greeting.delete_greeting(dict_parameter):
+			return HttpResponse(status=204)
+		else:
+			return HttpResponse(status=404)
--- guestbook/guestbookapp/models.py
+++ guestbook/guestbookapp/models.py
@@ -1,133 +1,148 @@
 from google.appengine.ext import ndb
 from google.appengine.api import users
 from google.appengine.datastore.datastore_query import Cursor
-
+from google.appengine.ext.db import Error
 # We set a parent key on the 'Greetings' to ensure that they are all in the same
 # entity group. Queries across the single entity group will be consistent.
 # However, the write rate should be limited to ~1/second.
 
 
 class AppConstants(object):
-
-    @property
-    def get_default_guestbook_name(self):
-        return "default_guestbook"
+	@property
+	def get_default_guestbook_name(self):
+		return "default_guestbook"
 
 
 class GuestBook(ndb.Model):
-    """Models an individual GuestBook entry."""
-    name = ndb.StringProperty(indexed=True)
+	"""Models an individual GuestBook entry."""
+	name = ndb.StringProperty(indexed=True)
 
-    @staticmethod
-    def get_guestbook(guestbook_name):
-        try:
-            return GuestBook.query(GuestBook.name == guestbook_name).get()
-        except (RuntimeError, ValueError):
-            return None
+	@staticmethod
+	def get_guestbook(guestbook_name):
+		try:
+			return GuestBook.query(GuestBook.name == guestbook_name).get()
+		except (RuntimeError, ValueError):
+			return None
 
-    @staticmethod
-    def add_guestbook(guestbook_name):
-        try:
-            guestbook = GuestBook()
-            guestbook.name = guestbook_name
-            guestbook.put()
-            return True
-        except (RuntimeError, TypeError):
-            return False
+	@staticmethod
+	def add_guestbook(guestbook_name):
+		try:
+			guestbook = GuestBook()
+			guestbook.name = guestbook_name
+			guestbook.put()
+			return True
+		except (RuntimeError, TypeError):
+			return False
 
 
 def get_guestbook_key(guestbook_name=AppConstants().get_default_guestbook_name):
-    return ndb.Key('GuestBook', guestbook_name)
+	return ndb.Key('GuestBook', guestbook_name)
 
 
 def is_exist(guestbook_name):
-    """
+	"""
 
-    :rtype : Guestbook
-    """
-    if GuestBook.get_guestbook(guestbook_name) is None:
-        return False
-    else:
-        return True
+	:rtype : Guestbook
+	"""
+	if GuestBook.get_guestbook(guestbook_name) is None:
+		return False
+	else:
+		return True
 
 
 class Greeting(ndb.Model):
-    """Models an individual GuestBook entry."""
-    author = ndb.UserProperty()
-    content = ndb.StringProperty(indexed=False)
-    date = ndb.DateTimeProperty(auto_now_add=True)
+	"""Models an individual GuestBook entry."""
+	author = ndb.UserProperty()
+	content = ndb.StringProperty(indexed=False)
+	date = ndb.DateTimeProperty(auto_now_add=True)
+	update_by = ndb.UserProperty()
+	update_date = ndb.DateTimeProperty(auto_now_add=False)
 
-    @classmethod
-    def get_latest(cls, guestbook_name, count, cursor):
-        """
+	@classmethod
+	def get_latest(cls, guestbook_name, count, cursor):
+		"""
 
-        :type cls: Greeting
-        """
-        curs = Cursor(urlsafe=cursor)
-        greets, next_curs, more = cls.query(ancestor=get_guestbook_key(guestbook_name)).fetch_page(count, start_cursor=curs)
-        return greets, next_curs, more
+		:type cls: Greeting
+		"""
+		curs = Cursor(urlsafe=cursor)
+		greets, next_curs, more = cls.query(ancestor=get_guestbook_key(guestbook_name)).fetch_page(
+			count, start_cursor=curs).order(-Greeting.date)
+		return greets, next_curs, more
 
-    @classmethod
-    def get_greeting(cls, guestbook_name, greeting_id):
-        return cls.query(cls.key == ndb.Key("GuestBook", str(guestbook_name), "Greeting",
-                                            int(greeting_id))).get()
+	@classmethod
+	def get_greeting(cls, guestbook_name, greeting_id):
+		return cls.query(
+			ndb.Key("GuestBook", str(guestbook_name), "Greeting",
+					int(greeting_id)) == cls.key).get()
 
-    @classmethod
-    def put_from_dict(cls, dictionary):
-        guestbook_name = dictionary.get("guestbook_name")
-        if not guestbook_name:
-            return False
-        else:
-            try:
-                if is_exist(guestbook_name) is False:
-                    GuestBook.add_guestbook(guestbook_name)
-                greeting = cls(parent=get_guestbook_key(guestbook_name))
-                if users.get_current_user():
-                    greeting.author = users.get_current_user()
-                greeting.content = dictionary.get("greeting_message")
-                greeting.put()
-                return True
-            except StandardError:
-                return False
+	@classmethod
+	def put_from_dict(cls, dictionary):
+		guestbook_name = dictionary.get("guestbook_name")
+		if not guestbook_name:
+			return False
+		else:
+			try:
+				if is_exist(guestbook_name) is False:
+					GuestBook.add_guestbook(guestbook_name)
+				greeting = cls(parent=get_guestbook_key(guestbook_name))
+				if users.get_current_user():
+					greeting.author = users.get_current_user()
+				greeting.content = dictionary.get("greeting_message")
+				greeting.put()
+				return greeting
+			except Error:
+				return None
 
-    @classmethod
-    def edit_greeting(cls, dictionary):
-        greeting_id = dictionary.get("greeting_id")
-        greeting_content = dictionary.get("greeting_message")
-        guestbook_name = dictionary.get("guestbook_name")
-        if not guestbook_name:
-            guestbook_name = AppConstants().get_default_guestbook_name
-        try:
-            if is_exist(guestbook_name) is False:
-                GuestBook.add_guestbook(guestbook_name)
-                greeting = cls(parent=get_guestbook_key(guestbook_name))
-                if users.get_current_user():
-                    greeting.author = users.get_current_user()
-            else:
-                greeting = cls.query(
-                    Greeting.key == ndb.Key("GuestBook", guestbook_name, "Greeting",
-                                            int(greeting_id))).get()
-                if greeting is None:
-                    greeting = cls(parent=get_guestbook_key(guestbook_name))
-                    if users.get_current_user():
-                        greeting.author = users.get_current_user()
-            greeting.content = greeting_content
-            greeting.put()
-            return True
-        except StandardError:
-            return False
+	@classmethod
+	def to_dict(cls):
+		item = {
+			'id': str(cls.key.id()),
+			'author': str(cls.author),
+			'content': cls.content,
+			'date': str(cls.date),
+			'updated_by': str(cls.update_by),
+			'updated_date': str(cls.update_date),
+		}
+		return item
 
-    @classmethod
-    def delete_greeting(cls, dictionary):
-        greeting_id = dictionary.get("id")
-        guestbook_name = dictionary.get("guestbook_name")
-        try:
-            greeting = cls.query(Greeting.key == ndb.Key("GuestBook", guestbook_name, "Greeting",
-                                                         int(greeting_id))).get()
-            if greeting is None:
-                return False
-            else:
-                greeting.key.delete()
-                return True
-        except StandardError:
-            return False
+	@classmethod
+	def edit_greeting(cls, dictionary):
+		greeting_id = dictionary.get("greeting_id")
+		greeting_content = dictionary.get("greeting_message")
+		guestbook_name = dictionary.get("guestbook_name")
+		if not guestbook_name:
+			guestbook_name = AppConstants().get_default_guestbook_name
+		try:
+			if is_exist(guestbook_name) is False:
+				GuestBook.add_guestbook(guestbook_name)
+				greeting = cls(parent=get_guestbook_key(guestbook_name))
+				if users.get_current_user():
+					greeting.author = users.get_current_user()
+			else:
+				greeting = cls.query(
+					Greeting.key == ndb.Key("GuestBook", guestbook_name, "Greeting",
+											int(greeting_id))).get()
+				if greeting is None:
+					greeting = cls(parent=get_guestbook_key(guestbook_name))
+					if users.get_current_user():
+						greeting.author = users.get_current_user()
+			greeting.content = greeting_content
+			greeting.put()
+			return True
+		except Error:
+			return False
+
+	@classmethod
+	def delete_greeting(cls, dictionary):
+		greeting_id = dictionary.get("id")
+		guestbook_name = dictionary.get("guestbook_name")
+		try:
+			greeting = cls.query(Greeting.key == ndb.Key("GuestBook", guestbook_name, "Greeting",
+														 int(greeting_id))).get()
+			if greeting is None:
+				return False
+			else:
+				greeting.key.delete()
+				return True
+		except Error:
+			return False
