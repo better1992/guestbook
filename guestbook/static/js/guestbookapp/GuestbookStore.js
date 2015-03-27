522db98f907db7472b096e059c5cc345101955fc
better1992 <vthanhtam42@gmail.com>
2015-03-27 17:54:39 +0700

    change some code

--- guestbook/static/js/guestbookapp/GuestbookStore.js
+++ guestbook/static/js/guestbookapp/GuestbookStore.js
@@ -1,19 +1,19 @@
 define([
-		"dojo/_base/declare",
-		"dojo/_base/lang",
-		"dojo/store/JsonRest",
-		"dojo/store/Cache",
-		"dojo/store/Memory",
-		"dojo/store/Observable",
-		"dojo/cookie"
-   ], function(declare, lang, JsonRest, Cache, Memory, Observable, _cookie) {
-	return declare([], {
+	"dojo/_base/declare",
+	"dojo/_base/lang",
+	"dojo/store/JsonRest",
+	"dojo/store/Cache",
+	"dojo/store/Memory",
+	"dojo/store/Observable",
+	"dojo/cookie"
+   ], function(declare, lang, JsonRest, Cache, Memory, _cookie) {
+	return declare('GuestbookStore', [], {
 		guestbookStore : null,
 		target: "/guestbookapp/api/guestbook/{0}/greeting/",
 
 		constructor: function (data) {
             this.guestbook_name = data.guestbook_name;
-			var GuestbookMemoryStore = new Observable(new Memory());
+			var GuestbookMemoryStore = new Memory();
 			this.target = lang.replace(this.target, [this.guestbook_name]);
 			var GuestbookJsonRestStore = new JsonRest(
 				{ target: this.target, headers: {"X-CSRFToken": _cookie('csrftoken')}}
--- guestbook/static/js/guestbookapp/view/GuestbookView.js
+++ guestbook/static/js/guestbookapp/view/GuestbookView.js
@@ -1,50 +1,54 @@
 define([
-		"dojo/_base/declare",
-		"dojo/_base/lang",
-		"dojo/dom",
-		"dojo/dom-construct",
-		"dojo/_base/array",
-		"dojo/on",
-		"dojo/text!./templates/GuestbookWidget.html",
-		"dijit/layout/ContentPane",
-		"dijit/form/ValidationTextBox",
-		"dijit/form/Button",
-		"dijit/form/Form",
-		"dijit/Dialog",
-		"dijit/form/TextBox",
-		"guestbookapp/view/GreetingView",
-		"guestbookapp/view/_ViewBaseMixin",
-		"dojo/domReady!"
-	   ], function(declare, lang, dom, domConstruct, array, on, template, contentpane,  validtextbox, button, form,
-				     dialog, textbox, GreetingView, _ViewBaseMixin){
+	"dojo/_base/declare",
+	"dojo/_base/lang",
+	"dojo/_base/array",
+	"dojo/_base/config",
+	"dojo/dom",
+	"dojo/dom-construct",
+	"dojo/on",
+	"dojo/text!./templates/GuestbookView.html",
+	"dijit/layout/ContentPane",
+	"dijit/form/ValidationTextBox",
+	"dijit/form/Button",
+	"dijit/form/Form",
+	"dijit/Dialog",
+	"dijit/form/TextBox",
+	"guestbookapp/GuestbookStore",
+	"guestbookapp/view/GreetingView",
+	"guestbookapp/view/_ViewBaseMixin",
+	"dojo/domReady!"
+	   ], function(declare, lang, array, config, dom, domConstruct, on, template, contentpane,  validtextbox, button, form,
+				     dialog, textbox, GuestbookStore, GreetingView, _ViewBaseMixin){
 	return declare('guestbookWidget', [_ViewBaseMixin], {
 		//	set our template
-		dict: null,
 		templateString: template,
-		guestbookStore: null,
+
 		//	some properties
+		guestbookStore: null,
 		guestbook_name : 'temp_guestbook',
-		isAdmin: 0,
-		currentUser: null,
 		greetings: [],
 
 		constructor: function (params) {
-            this.guestbook_name = params.guestbook_name;
-			this.guestbookStore = params.guestbookStore;
-			this.isAdmin = params.isAdmin;
-			this.currentUser = params.currentUser;
-        },
+            this.guestbook_name = config.guestbook_name;
+			//var guestbook_name = this.guestbook_name;
+			var store = new GuestbookStore({'guestbook_name': this.guestbook_name });
+        	this.guestbookStore = store.guestbookStore;
+		},
 
 		postCreate: function(){
 			this.inherited(arguments);
-			this.own( on(this.get_list, "click", lang.hitch(this, "getList")),
-					  on(this.sign, "click", lang.hitch(this, "signGreeting")),
-					  on(this.delete_all, "click", lang.hitch(this, "deleteAll")),
-					  on(this.ok_detail, "click", lang.hitch(this, "hidePopup"))
-					);
+
+
+			this.own(
+				on(this.get_listNode, "click", lang.hitch(this, "getList")),
+				on(this.signNode, "click", lang.hitch(this, "signGreeting")),
+				on(this.delete_allNode, "click", lang.hitch(this, "deleteAll")),
+				on(this.ok_detailNode, "click", lang.hitch(this, "hidePopup"))
+			);
 		},
 
 		getList: function() {
+			this.deleteAll();
 			var guestbook_name = dijit.byId('guestbook').get('value');
 			this.guestbookStore.query().then(lang.hitch(this, function(result){
 				var greetings = dom.byId('greetingContainer');
@@ -55,19 +59,17 @@ define([
 						data = {
 							guestbookWidget: this,
 							guestbook_name: guestbook_name,
-							isAdmin: this.isAdmin,
-							currentUser: this.currentUser,
 							id: greeting.id,
 							author: greeting.author,
 							content: greeting.content
 						}
 						var widget = new GreetingView(data);
-						widget.placeAt(greetings).startup();
-						this.greetings.push(widget);
+						widget.placeAt(greetings)
+						widget.startup();
 						console.log(this.greetings);
 					}));
-					if(this.isAdmin) {
-						dojo.style(this.delete_div, 'display', '');
+					if(config.isAdmin) {
+						dojo.style(this.delete_divNode, 'display', '');
 					}
 				}
 			}));
@@ -75,17 +77,16 @@ define([
 		},
 
 		hidePopup: function(){
-			this.detail_dialog.hide();
+			this.detail_dialogNode.hide();
 		},
 
 
 		signGreeting: function() {
 			postData = {
-					guestbook_name: dijit.byId('guestbook').get('value'),
-					greeting_message: dijit.byId('content_dialog').get('value')
+					guestbook_name: this.guestbook_nameNode.get('value'),
+					greeting_message: this.content_dialog_signNode.get('value')
 				}
 			this.guestbookStore.add(postData);
-			//alert(123);
 		},
 
 		deleteAll: function() {
@@ -96,4 +97,4 @@ define([
 		}
 
 	});
-});
+});;
