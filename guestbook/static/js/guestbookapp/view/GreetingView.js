8cef276f39432097548e206b7c76f757aa392d1f
better1992 <vthanhtam42@gmail.com>
2015-03-27 17:53:39 +0700

    change some code

--- guestbook/static/js/guestbookapp/view/GreetingView.js
+++ guestbook/static/js/guestbookapp/view/GreetingView.js
@@ -1,7 +1,7 @@
 define([
 		"dojo/_base/declare",
 		"dojo/_base/lang",
-		"dojo/_base/Deferred",
+		"dojo/_base/config",
 		"dojo/dom",
 		"dojo/dom-attr",
 		"dojo/dom-style",
@@ -10,14 +10,14 @@ define([
 		"dojo/request",
 		"dojo/on",
 		"dojo/store/Memory",
-		"dojo/text!./templates/GreetingWidget.html",
+		"dojo/text!./templates/GreetingView.html",
 		"dijit/form/ValidationTextBox",
 		"dijit/form/Button",
 		"dijit/form/Form",
 		"dijit/Dialog",
 		"dijit/form/TextBox",
 		"guestbookapp/view/_ViewBaseMixin",
-	   ], function(declare, lang, Deferred, dom, domAttr, domstyle, cookie, when, request, on, Memory, template, validtextbox, button, form, dialog, textbox, _ViewBaseMixin){
+	   ], function(declare, lang, config, dom, domAttr, domstyle, cookie, when, request, on, Memory, template, validtextbox, button, form, dialog, textbox, _ViewBaseMixin){
 	return declare("greetingWidget",[_ViewBaseMixin], {
 		//	set our template
 		templateString: template,
@@ -25,14 +25,10 @@ define([
 		//	some properties
 		id: 123,
 		author: 'test@gmail.com',
-        content: 'demo',
+		content: 'demo',
 		guestbook_name: '',
 		guestbookStore: null,
-		isAdmn: 0,
-		currentUser: null,
 		guestbookWidget: null,
-		dialog: null,
-		updated: false,
 
         constructor: function (params) {
 			this.guestbookWidget = params.guestbookWidget
@@ -41,74 +37,63 @@ define([
             this.content = params.content;
 			this.id = params.id;
 			this.guestbookStore = this.guestbookWidget.guestbookStore
-			this.tempStore = this.guestbookStore;
         },
 
 		postCreate: function(){
 			this.inherited(arguments);
 
-			if (this.isAdmin == 0){
-				dojo.style(this.option, 'display', 'None');
+			if (config.isAdmin == 0){
+				dojo.style(this.optionNode, 'display', 'None');
 			}
-            this.own(on(this.delete, 'click', lang.hitch(this, 'deleteWidget')),
-					on(this.show_Edit, 'click', lang.hitch(this, 'showEdit')),
-					on(this.Cancel_Edit, 'click', lang.hitch(this, 'cancelEdit')),
-					on(this.Ok_Edit, 'click', lang.hitch(this, 'okEdit')),
-					on(this.show_detail, 'click', lang.hitch(this, 'showDetail')));
+            this.own(
+				on(this.deleteNode, 'click', lang.hitch(this, 'deleteNodeWidget')),
+				on(this.show_EditNode, 'click', lang.hitch(this, 'showEdit')),
+				on(this.Cancel_EditNode, 'click', lang.hitch(this, 'cancelEdit')),
+				on(this.Ok_EditNode, 'click', lang.hitch(this, 'okEdit')),
+				on(this.show_detailNode, 'click', lang.hitch(this, 'showDetail'))
+			);
 		},
 
-		refreshStore: function(){
-				this.guestbookStore = this.tempStore;
-				//return this.guestbookStore
-		},
-
-        deleteWidget: function(){
+        deleteNodeWidget: function(){
             this.guestbookStore.remove(this.id);
 			this.destroyRecursive();
 
         },
 		showEdit: function(){
 				this.contentNode.set('readonly', false);
-				dojo.style(this.option, 'display', 'None');
-				dojo.style(this.edit_option, 'display', '');
+				dojo.style(this.optionNode, 'display', 'None');
+				dojo.style(this.edit_optionNode, 'display', '');
 		},
 
 		cancelEdit: function(){
 				this.contentNode.set('readonly', true);
-				dojo.style(this.edit_option, 'display', 'None');
-				dojo.style(this.option, 'display', '');
+				dojo.style(this.edit_optionNode, 'display', 'None');
+				dojo.style(this.optionNode, 'display', '');
 		},
 
 		okEdit : function() {
 			this.cancelEdit();
 			putData = {
 					id: this.id,
-					updated_by: this.currentUser,
+					updated_by: config.currentUser,
 					guestbook_name: this.guestbook_name,
 					greeting_message: this.contentNode.get('value')
 				}
-			//console.log(this.id);
 			this.guestbookStore.put(putData);
 
 		},
 
 		showDetail: function() {
-
-			when(this.guestbookStore.get(this.id)).then(lang.hitch(this, function (greeting) {
-
-				this.guestbookWidget.ID.innerHTML = greeting['object'].id;
-				this.guestbookWidget.guestbook_name_dialog.innerHTML = greeting['object'].guestbook_name;
-				this.guestbookWidget.author_dialog.innerHTML = greeting['object'].author;
-				this.guestbookWidget.content_dialog.innerHTML = greeting['object'].content;
-				this.guestbookWidget.date_dialog.innerHTML = greeting['object'].date;
-				this.guestbookWidget.updated_by_dialog.innerHTML = greeting['object'].updated_by;
-				this.guestbookWidget.updated_date_dialog.innerHTML = greeting['object'].updated_date;
-
-				console.log(greeting);
+			this.guestbookStore.get(this.id).then(lang.hitch(this, function (greeting) {
+				this.guestbookWidget.IDNode.innerHTML = greeting['object'].id;
+				this.guestbookWidget.guestbook_name_dialogNode.innerHTML = greeting['object'].guestbook_name;
+				this.guestbookWidget.author_dialogNode.innerHTML = greeting['object'].author;
+				this.guestbookWidget.content_dialogNode.innerHTML = greeting['object'].content;
+				this.guestbookWidget.date_dialogNode.innerHTML = greeting['object'].date;
+				this.guestbookWidget.updated_by_dialogNode.innerHTML = greeting['object'].updated_by;
+				this.guestbookWidget.updated_date_dialogNode.innerHTML = greeting['object'].updated_date;
 			}));
-
-			this.guestbookWidget.detail_dialog.show();
-			//console.log(this.guestbookStore);
+			this.guestbookWidget.detail_dialogNode.show();
 		}
 
 
