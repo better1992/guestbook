ac467123144a14a1f01df1fd347d925a348dd7ec
tamvt <tamvt@aoi-sys.vn>
2015-07-09 13:25:28 +0700

    fix comment

--- guestbook/guestbookapp/api.py
+++ guestbook/guestbookapp/api.py
@@ -1,11 +1,12 @@
 import json
 
-from django.http import HttpResponse
+from django.http import HttpResponse, QueryDict
 from django.views.generic import FormView
 from django.views.generic.list import BaseListView
 from django.views.generic.detail import BaseDetailView
 
 from services import GreetingService
+
 
 class JSONResponseMixin(object):
 	def render_to_response(self, context, **response_kwargs):
@@ -19,26 +20,52 @@ class JSONResponseMixin(object):
 
 
 class GreetingCollectionView(JSONResponseMixin, BaseListView, FormView):
-
 	# handle GET request: get list messages and return as JSON
 	def get_queryset(self):
-		return GreetingService.list(self.request, self.kwargs)
+		cursor = self.request.GET.get("cursor", None)
+		guestbook_name = self.kwargs.get("guestbook_name")
+		return GreetingService.list(guestbook_name=guestbook_name, cursor=cursor)
 
 	# handle POST request: create message and return HttpStatus code
 	def post(self, request, **kwargs):
-		return GreetingService.post(request, **kwargs)
+		try:
+			json_object = json.loads(request.body)
+		except ValueError:
+			# invalid json
+			request.POST = QueryDict(request.body)
+		else:
+			# valid json
+			request.POST = json_object
+		content = request.POST.get('greeting_message')
+		guestbook_name = kwargs.get('guestbook_name')
+		return GreetingService.create(guestbook_name=guestbook_name, content=content)
 
 
 class GreetingSingleView(JSONResponseMixin, BaseDetailView, FormView):
-
 	# handle GET request: get a message and return HttpStatus code
 	def get_object(self, request, **kwargs):
-		return GreetingService.get(request, **kwargs)
+		greeting_id = kwargs.get("id")
+		guestbook_name = kwargs.get("guestbook_name")
+		return GreetingService.get(guestbook_name=guestbook_name, pk=greeting_id)
 
 	# handle PUT request: Edit a message and return HttpStatus code
 	def put(self, request, **kwargs):
-		return GreetingService.put(request, **kwargs)
+		try:
+			json_object = json.loads(request.body)
+		except ValueError:
+			# invalid json
+			request.PUT = QueryDict(request.body)
+		else:
+			# valid json
+			request.PUT = json.loads(request.body)
+		guestbook_name = kwargs.get("guestbook_name")
+		greeting_id = kwargs.get("id")
+		content = request.PUT.get("greeting_message")
+		return GreetingService.update(guestbook_name=guestbook_name, content=content,
+										pk=greeting_id)
 
 	# handle DELETE request: Delete a message and return HttpStatus code
 	def delete(self, request, *args, **kwargs):
-		return GreetingService.delete(**kwargs)
+		id = kwargs.get("id"),
+		guestbook_name = kwargs.get("guestbook_name")
+		return GreetingService.delete(guestbook_name=guestbook_name, pk=id[0])
--- guestbook/guestbookapp/services.py
+++ guestbook/guestbookapp/services.py
@@ -1,19 +1,19 @@
 import json
 
 from django.http import HttpResponse
-from django.http import Http404, QueryDict
+from django.http import Http404
 
-from models import Greeting
+from models import Greeting, AppConstants
 
+GUESTBOOK_NAME = AppConstants().get_default_guestbook_name
 
 class GreetingService(object):
 
 	# handle GET request: get list messages and return as JSON
 	@classmethod
-	def list(cls, request, kwargs):
-		cursor = request.GET.get("cursor", None)
-		guestbook_name = kwargs.get("guestbook_name")
-		greetings, next_cursor, more = Greeting.get_latest(guestbook_name, 20, cursor)
+	def list(cls, guestbook_name=GUESTBOOK_NAME, limit=10, cursor=None, **kwargs):
+
+		greetings, next_cursor, more = Greeting.get_latest(guestbook_name, limit, cursor)
 		if greetings is None:
 			return HttpResponse(status=404)
 		lst_greeting = [greeting.to_dict() for greeting in greetings]
@@ -33,24 +33,15 @@ class GreetingService(object):
 
 	# handle POST request: create message and return HttpStatus code
 	@classmethod
-	def post(cls, request, **kwargs):
-		try:
-			json_object = json.loads(request.body)
-		except ValueError:
-			# invalid json
-			request.POST = QueryDict(request.body)
-		else:
-			# valid json
-			request.POST = json_object
-
-		guestbook_name = kwargs.get('guestbook_name')
-		if (guestbook_name is None) | (request.POST.get('greeting_message') is None):
+	def create(cls, guestbook_name=GUESTBOOK_NAME, content=None, **kwargs):
+		import logging
+		logging.warning(guestbook_name)
+		if content is None:
 			return HttpResponse(status=400)
 		dict_parameter = {
 			'guestbook_name': guestbook_name,
-			'greeting_message': request.POST.get('greeting_message')
+			'greeting_message': content
 		}
-
 		if Greeting.put_from_dict(dict_parameter):
 			return HttpResponse(status=204)
 		else:
@@ -58,11 +49,9 @@ class GreetingService(object):
 
 	# handle GET request: get a message and return HttpStatus code
 	@classmethod
-	def get(cls, **kwargs):
-		greeting_id = kwargs.get("id")
-		guestbook_name = kwargs.get("guestbook_name")
+	def get(self, guestbook_name=GUESTBOOK_NAME, pk=None):
 
-		greeting = Greeting.get_greeting(guestbook_name, greeting_id)
+		greeting = Greeting.get_greeting(guestbook_name, pk)
 		if greeting is None:
 			return Http404
 
@@ -72,21 +61,12 @@ class GreetingService(object):
 
 	# handle PUT request: Edit a message and return HttpStatus code
 	@classmethod
-	def put(cls, request, **kwargs):
-		try:
-			json_object = json.loads(request.body)
-		except ValueError:
-			# invalid json
-			request.PUT = QueryDict(request.body)
-		else:
-			# valid json
-			request.PUT = json.loads(request.body)
+	def update(cls, guestbook_name=GUESTBOOK_NAME, pk=None, content=None, **kwargs):
 
 		dict_parameter = {
-			'guestbook_name': kwargs.get("guestbook_name"),
-			'greeting_id': kwargs.get("id"),
-			'greeting_message': request.PUT.get("greeting_message"),
-			'updated_by': request.PUT.get('updated_by'),
+			'guestbook_name': guestbook_name,
+			'greeting_id': pk,
+			'greeting_message': content,
 		}
 
 		if Greeting.edit_greeting(dict_parameter):
@@ -96,10 +76,10 @@ class GreetingService(object):
 
 	# handle DELETE request: Delete a message and return HttpStatus code
 	@classmethod
-	def delete(cls, **kwargs):
+	def delete(cls, guestbook_name=GUESTBOOK_NAME, pk=None):
 		dict_parameter = {
-			'id': kwargs.get("id"),
-			'guestbook_name': kwargs.get("guestbook_name")
+			'id': pk,
+			'guestbook_name': guestbook_name
 		}
 
 		if Greeting.delete_greeting(dict_parameter):
